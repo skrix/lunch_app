6923b75a28b29f14ab7e8e424f5562e69d6254b9
Andrew Shyika <andrew.shyika@gmail.com>
2018-07-27 16:13:57 +0300

    Fat scenarios fix

--- spec/features/signing_users_in_spec.rb
+++ spec/features/signing_users_in_spec.rb
@@ -4,9 +4,7 @@ RSpec.feature 'Users signin' do
   let(:valid_user)   { FactoryBot.build(:valid_user) }
   let(:invalid_user) { FactoryBot.build(:invalid_user_without_email) }
 
-
-
-  scenario 'with valid credentials' do
+  def sign_in(user)
     visit '/'
 
     click_link 'Sign in'
@@ -15,29 +13,25 @@ RSpec.feature 'Users signin' do
     fill_in 'Password', with: user.password
 
     click_button 'Log in'
+  end
+
+  scenario 'with valid credentials' do
+    sign_in(valid_user)
 
     expect(page).to have_content('Signed in successfully.')
-    expect(page).to have_content("Signed in as #{user.email}")
+    expect(page).to have_content("Signed in as #{valid_user.email}")
 
     expect(page).not_to have_link('Sign in')
     expect(page).not_to have_link('Sign up')
   end
 
   scenario 'with invalid credentials' do
-    visit '/'
-
-    click_link 'Sign in'
-
-    fill_in 'Email',    with: invalid_user.email
-    fill_in 'Password', with: invalid_user.password
-
-    click_button 'Log in'
+    sign_in(invalid_user)
 
     expect(page).to have_content('Invalid Email or password.')
 
     expect(page).to have_link('Sign in')
     expect(page).to have_link('Sign up')
     expect(page).to have_link('Forgot your password?')
-
   end
 end
