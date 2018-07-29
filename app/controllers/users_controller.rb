4916fd734a72f726140f2c7ff5f5738b8d20ba1a
Andrew Shyika <andrew.shyika@gmail.com>
2018-07-29 14:22:05 +0300

    Fixes according to commetns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, except: %i[new create]
 
   # GET /users
   # GET /users.json
@@ -9,17 +9,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show; end
-
   # GET /users/new
   def new
     @user = User.new
   end
-
-  # GET /users/1/edit
-  def edit; end
 
   # POST /users
   # POST /users.json
@@ -29,13 +22,6 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html { redirect_to @user, notice: 'User was successfully created.' } if @user.save
       format.html { render :new } unless @user.save
-      # if @user.save
-      #   format.html { redirect_to @user, notice: 'User was successfully created.' }
-      #   format.json { render :show, status: :created, location: @user }
-      # else
-      #   format.html { render :new }
-      #   format.json { render json: @user.errors, status: :unprocessable_entity }
-      # end
     end
   end
 
@@ -46,13 +32,6 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html { redirect_to @user, notice: successfully } if @user.update(user_params)
       format.html { render :edit } unless @user.update(user_params)
-      # if @user.update(user_params)
-      #   format.html { redirect_to @user, notice: 'User was successfully updated.' }
-      #   format.json { render :show, status: :ok, location: @user }
-      # else
-      #   format.html { render :edit }
-      #   format.json { render json: @user.errors, status: :unprocessable_entity }
-      # end
     end
   end
 
@@ -62,7 +41,6 @@ class UsersController < ApplicationController
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
