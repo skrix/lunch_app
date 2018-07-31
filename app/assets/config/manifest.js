0237dcb68d5ae5cff7c9fa0e4649c7d2b67cd223
Andrew Shyika <andrew.shyika@gmail.com>
2018-07-31 11:31:40 +0300

    Remove SASS require in manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-# SASS
-*= require devise_bootstrap_views
-
 # LESS
 *= require devise_bootstrap_views_less
