acae93c2d39b9069f2e2cee3bc8a791783fa5f76
Andrew Shyika <andrew.shyika@gmail.com>
2018-07-31 16:22:41 +0300

    User email length validation delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
-EMAIL_LENGTH_RANGE = 6..40
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, length: { in: EMAIL_LENGTH_RANGE }
   validates :email, uniqueness: true
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,7 +50,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
