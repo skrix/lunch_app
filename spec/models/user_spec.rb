3c133de14bbff3b7df67a8aa70b351ffa2205bd4
Andrew Shyika <andrew.shyika@gmail.com>
2018-07-31 12:22:22 +0300

    Describe User model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  it "is valid with valid attributes"
+
+  it "is not valid without a email"
+  it "is not valid without a password"
+  it "is not valid without a password_confirmation"
+
+  it "is not valid with invalid email"
+  it "is not valid with too long email"
+  it "is not valid with too short email"
+
+  it "is not valid with invalid password"
+  it "is not valid with too long password"
+  it "is not valid with too short password"
 end
