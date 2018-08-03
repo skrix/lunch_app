b63f68c0d2daa1a2089dd35c4a503d3a5cf4839f
Andrew <andrew.shyika@gmail.com>
2018-08-03 12:08:05 +0300

    3 - Add rolify to users and set first user as lunch_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, uniqueness: true
+
+  before_create :assign_role
+
+  private
+
+  def assign_role
+    add_role(:lunch_admin) unless User.exists?
+  end
 end
