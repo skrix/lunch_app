a1fc8304ecc3150fa436d33313d5cd3ab7ece396
Andrew Shyika <andrew.shyika@gmail.com>
2018-08-03 12:41:09 +0300

    Add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit
+  protect_from_forgery
+
   before_action :authenticate_user!
   respond_to :html
 end
