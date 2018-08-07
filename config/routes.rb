8521a2e3e6ff4cc95509eb34735af6dd2d353944
Andrew Shyika <andrew.shyika@gmail.com>
2018-08-07 11:11:40 +0300

    Item generate

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :items
   resources :menus
   resources :users
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :item do
+    name "MyString"
+    price "9.99"
+  end
+end
