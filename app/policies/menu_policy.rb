9357358360eae6436ab83aee5c4028f0767678b8
Andrew Shyika <andrew.shyika@gmail.com>
2018-08-20 12:55:18 +0300

    Add policy new menu create only on weekaday

--- app/policies/menu_policy.rb
+++ app/policies/menu_policy.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class MenuPolicy < ApplicationPolicy
+  def create?
+    workday? && lunch_admin?
+  end
+
+  private
+
+  def workday?
+    Date.today.on_weekday?
+  end
+
   alias_method :index?,  :trust
   alias_method :show?,   :trust
   alias_method :new?,    :lunch_admin?
   alias_method :update?, :lunch_admin?
-  alias_method :create?, :lunch_admin?
 
-  public :index?, :create?, :update?,
+  public :index?, :update?,
          :show?,  :new?
 end
